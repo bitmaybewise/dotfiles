a925f99caaafd55c01445addcd1a0942d0760065
Hercules Merscher <hlmerscher@gmail.com>
2013-03-04 10:41:39 -0300

    automatizacoes p/ rails

---
+++ .rails/rails_autoconf.rb
@@ -0,0 +1,25 @@
+# gemfile
+gsub_file "Gemfile", /(gem 'coffee-rails', .*)/, "# \\1"
+
+# clean up rails defaults
+remove_file "public/images/rails.png"
+remove_file "public/index.html"
+
+# config. rails security
+run "cp config/database.yml config/database.example"
+run "cp config/initializers/secret_token.rb config/initializers/secret_token.example"
+append_file "config/initializers/secret_token.example", "# run 'rake secret' to generate a new token for session"
+append_file ".gitignore", "config/database.yml"
+append_file ".gitignore", "config/initializers/secret_token.rb"
+
+rake "secret"
+run "bundle install"
+
+# prepare db
+rake "db:create", :env => "development"
+rake "db:migrate", :env => "development"
+
+# creating repo and first commit
+git :init
+git :add => "."
+git :commit => "-m 'first!!!'"
