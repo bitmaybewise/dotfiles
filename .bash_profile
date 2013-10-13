4670ce4ad1e22e88e564fe1812005d22309b45f5
Hercules Merscher <hlmerscher@gmail.com>
2013-10-13 18:27:19 -0300

    .bash_profile

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+export PS1='\[\033[1;32m\]\h\[\033[1;34m\]::\[\033[1;31m\]\u\[\033[1;34m\]::\w\e[00m\n\$ '
--- configure.sh
+++ configure.sh
@@ -7,3 +7,7 @@ echo .gitconfig configurado!
 mv ~/.railsrc ~/.railsrc_old 2> /dev/null
 ln -s $(pwd)/.railsrc ~/.railsrc
 echo .railsrc configurado!
+
+mv ~/.bash_profile ~/.bash_profile_old 2> /dev/null
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+echo .bash_profile configurado!
