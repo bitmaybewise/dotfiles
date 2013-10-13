bcd36f283b01115b1c29f4eaaa9f2e597d852acd
Hercules Merscher <hlmerscher@gmail.com>
2013-10-13 18:22:01 -0300

    removidas automações do rails new

--- configure.sh
+++ configure.sh
@@ -7,7 +7,3 @@ echo .gitconfig configurado!
 mv ~/.railsrc ~/.railsrc_old 2> /dev/null
 ln -s $(pwd)/.railsrc ~/.railsrc
 echo .railsrc configurado!
-
-mv ~/.rails ~/.rails_old 2> /dev/null
-ln -s $(pwd)/.rails ~/.rails
-echo .rails configurado!
